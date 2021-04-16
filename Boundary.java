722f445e6b1f5e447af6575962a60792e92bf185
JokerOfRivia <70218072+JokerOfRivia@users.noreply.github.com>
2021-04-16 09:29:34 -0700

    yes

--- Boundary.java
+++ Boundary.java
@@ -5,12 +5,19 @@ public class Boundary{
 
 	}
 	public static void squishA(){
-		double[] array= {-1337.1337, -10.5, -5.5, -1.5, -1.0, -0.3, -0.113, 0.0, 0.31, 0.76, 1.3, 1.99, 7.4, 160.0, 1337.0};
+		double[] array= {0, 0.000027535691114583473, 0.004070137715896128, 0.18242552380635635, 0.2689414213699951, 0.425557483188341, 0.47178002201963243, 0.5, 0.5768852611320463, 0.6813537337890256, 0.7858349830425586, 0.8797431375322491, 0.9993891206405656, 1, 1};
+		double n;
 		for (int i=0; i<array.length;i++){
-			System.out.println("Input: "+array[i]+" Output: "+Math.abs(1 / (1+(double)Math.pow(2.71,array[i]))));
+			if (array[i]<=0.5){
+				n=array[i];
+			}
+			else{
+				n=1-array[i];
+			}
+			System.out.println("Input: "+array[i]+" Output: "+(n/2));
 		}
 	}
 	public static void main (String [] args){
-		index.squishA();
+		Boundary.squishA();
 	}
 }
